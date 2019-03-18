6ea04467b63ec5877f70eefe77aee7a7c5271f7b
João Neto <joao.alexandre@cesar.org.br>
2019-03-18 10:33:44 -0300

    Add GET data endpoint
    Change-Id: Ic679521ba4e919c821130cdf742397226f997b2a

--- src/data/MongoDatabase.js
+++ src/data/MongoDatabase.js
@@ -1,4 +1,10 @@
 import mongoose from 'mongoose';
+import moment from 'moment';
+import _ from 'lodash';
+
+function getISODate(date) {
+  return `${moment(date).format('YYYY-MM-DDTHH:mm:ss')}Z`;
+}
 
 class MongoDatabase {
   constructor(settings) {
@@ -14,6 +20,31 @@ class MongoDatabase {
     const Model = this.mongoose.model(name, schema);
     const dataModel = new Model(data);
     return dataModel.save();
+  }
+
+  async find(name, schema, query, options) {
+    const Model = this.mongoose.model(name, schema);
+    const queryBase = query;
+    const queryOptions = options;
+
+    if (queryBase.from && queryBase.sensorId) {
+      const sensorId = parseInt(queryBase.sensorId, 10);
+      const combinedQuery = { $and: [{ from: queryBase.from }, { 'payload.sensorId': sensorId }] };
+      return Model.find(combinedQuery, { _id: 0, __v: 0 }, queryOptions).exec();
+    }
+
+    if (queryOptions.orderBy) {
+      queryOptions.sort = {};
+      queryOptions.sort[queryOptions.orderBy] = queryOptions.order;
+    }
+    if (queryOptions.start) {
+      _.merge(queryBase, { timestamp: { $gte: getISODate(queryOptions.start) } });
+    }
+    if (queryOptions.finish) {
+      _.merge(queryBase, { timestamp: { $lte: getISODate(queryOptions.finish) } });
+    }
+
+    return Model.find(queryBase, { _id: 0, __v: 0 }, queryOptions).exec();
   }
 }
 
