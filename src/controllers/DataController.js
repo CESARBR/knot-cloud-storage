f5a72ec572d51173a49807e655ff57f2e71f7097
João Neto <joao.alexandre@cesar.org.br>
2019-03-18 10:33:44 -0300

    Add GET data endpoint
    Change-Id: Ic679521ba4e919c821130cdf742397226f997b2a

--- src/controllers/DataController.js
+++ src/controllers/DataController.js
@@ -1,6 +1,7 @@
 class DataController {
-  constructor(saveDataInteractor) {
+  constructor(saveDataInteractor, listDataInteractor) {
     this.saveDataInteractor = saveDataInteractor;
+    this.listDataInteractor = listDataInteractor;
   }
 
   async save(request, h) {
@@ -10,6 +11,18 @@ class DataController {
     } catch (err) {
       return h.response().code(400);
     }
+  }
+
+  async list(request, h) {
+    const data = await this.listDataInteractor.execute(request.query);
+    return h.response(data).code(200);
+  }
+
+  async listByDevice(request, h) {
+    const dataQuery = request.query;
+    dataQuery.from = request.params.id;
+    const data = await this.listDataInteractor.execute(dataQuery);
+    return h.response(data).code(200);
   }
 }
 
