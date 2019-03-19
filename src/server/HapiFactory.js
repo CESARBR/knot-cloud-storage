634f0be0873e7b626f1a58505292e2fb302d77bf
João Neto <joao.alexandre@cesar.org.br>
2019-03-19 18:43:34 -0300

    Add cloud capabilities
    Change-Id: I8fbb2bbd416e4cd0e91f0cea0de8c8cacf3cde2e

--- src/server/HapiFactory.js
+++ src/server/HapiFactory.js
@@ -4,18 +4,21 @@ import ListDataInteractor from 'interactors/ListData';
 import DataController from 'controllers/DataController';
 import DataStore from 'data/DataStore';
 import UuidAliasResolverFactory from 'network/UuidAliasResolverFactory';
+import CloudFactory from 'network/CloudFactory';
 
 class HapiFactory {
-  constructor(settings, database) {
+  constructor(settings, database, cloudRequester) {
     this.settings = settings;
     this.database = database;
+    this.cloudRequester = cloudRequester;
   }
 
   create() {
     const dataStore = new DataStore(this.database);
     const uuidAliasResolver = new UuidAliasResolverFactory(this.settings).create();
+    const cloud = new CloudFactory(this.cloudRequester, uuidAliasResolver).create();
     const saveDataInteractor = new SaveDataInteractor(dataStore, uuidAliasResolver);
-    const listDataInteractor = new ListDataInteractor(dataStore);
+    const listDataInteractor = new ListDataInteractor(dataStore, cloud);
     const dataController = new DataController(saveDataInteractor, listDataInteractor);
 
     return new Hapi(this.settings, dataController);
