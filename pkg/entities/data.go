9374a35b203b4bc5d9334ba24cec7d44d95d7b9e
Igor <igor@guaiuba-006888.local>
2020-03-17 15:30:18 -0300

    entity: data: Adds data body structure
    This commit adds the structure of the data. This structure will be used in the database, response body and in the post request body.

---
+++ pkg/entities/data.go
@@ -0,0 +1,14 @@
+package entities
+
+import "time"
+
+type Data struct {
+	From      string    `bson:"from" json:"from"`
+	Payload   Payload   `bson:"payload" json:"payload"`
+	Timestamp time.Time `bson:"timestamp" json:"timestamp"`
+}
+
+type Payload struct {
+	SensorId int `bson:"sensor_id" json:"sensor_id"`
+	Value    int `bson:"value" json:"value"`
+}
