207e4cc97a8df3f1cc0e4f0ea031911f18b6e2e1
Igor Albanezi Conceição <iac@cesar.org.br>
2020-04-02 15:18:13 -0300

    main: Adds the initialization of the instances
    This commits adds the initialization of all the instances and pass it to the server instance.

--- cmd/main.go
+++ cmd/main.go
@@ -2,17 +2,30 @@ package main
 
 import (
 	"github.com/CESARBR/knot-cloud-storage/internal/config"
+	"github.com/CESARBR/knot-cloud-storage/pkg/controllers"
+	"github.com/CESARBR/knot-cloud-storage/pkg/data"
+	"github.com/CESARBR/knot-cloud-storage/pkg/interactor"
 	"github.com/CESARBR/knot-cloud-storage/pkg/logging"
 	"github.com/CESARBR/knot-cloud-storage/pkg/server"
 )
 
 func main() {
 	config := config.Load()
-	logrus := logging.NewLogrus(config.Logger.Level)
 
+	logrus := logging.NewLogrus(config.Logger.Level)
 	logger := logrus.Get("Main")
 	logger.Info("Starting KNoT Cloud Storage")
 
-	server := server.NewServer(config.Server.Port, logrus.Get("Server"))
+	mongo := data.NewMongoDB(config.MongoDB.Host, config.MongoDB.Name)
+	database, err := mongo.Connect()
+	if err != nil {
+		logger.Error(err)
+	}
+
+	dataStore := data.NewDataStore(database, logrus.Get("Storage"))
+	dataInteractor := interactor.NewDataInteractor(dataStore, logrus.Get("Interactor"))
+	dataController := controllers.NewDataController(dataInteractor)
+
+	server := server.NewServer(config.Server.Port, logrus.Get("Server"), dataController)
 	server.Start()
 }
