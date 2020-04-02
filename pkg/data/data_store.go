c8aa3d927318dd0f40eec771c127419d294e5a4c
Igor Albanezi Conceição <iac@cesar.org.br>
2020-04-02 15:41:03 -0300

    storage: data: Implement the delete interaction
    - Adds the function that deletes the data directly from the database.
    - Adjusts the error handling by returning nil instead of returning
      always error

--- pkg/data/data_store.go
+++ pkg/data/data_store.go
@@ -14,6 +14,7 @@ const collection = "data"
 type IDataStore interface {
 	Get(order string, skip, take int, startDate, finishDate time.Time)
 	Save(data entities.Data)
+	Delete(deviceID string)
 }
 
 type Store struct {
@@ -40,19 +41,37 @@ func (ds *Store) Get(order string, skip, take int, startDate, finishDate time.Ti
 	}).Skip(skip).Sort(order).Limit(take).All(&data)
 	if err != nil {
 		ds.logger.Error(err)
+		return nil, err
 	}
 
 	if data == nil {
 		data = []entities.Data{}
 	}
 
-	return data, err
+	return data, nil
 }
 
 func (ds *Store) Save(data entities.Data) error {
 	err := ds.Database.C(collection).Insert(&data)
 	if err != nil {
 		ds.logger.Error(err)
+		return err
 	}
-	return err
+	return nil
+}
+
+func (ds *Store) Delete(deviceID string) error {
+	if deviceID == "" {
+		return ds.removeAll(nil)
+	}
+	return ds.removeAll(bson.M{"from": deviceID})
+}
+
+func (ds *Store) removeAll(query interface{}) error {
+	_, err := ds.Database.C(collection).RemoveAll(query)
+	if err != nil {
+		ds.logger.Error(err)
+		return err
+	}
+	return nil
 }
