2345bf3ae8e9b498fdd8624e8b9ca3a09007f673
Esdras Júnior <eblsj@cesar.org.br>
2020-04-08 14:56:43 -0300

    main: Initialize instance for amqp message handler
    This patch integrates the amqp message handler with the rest API server
    instances. Also adds Stop method to server.

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"os"
+
 	"github.com/CESARBR/knot-cloud-storage/internal/config"
 	"github.com/CESARBR/knot-cloud-storage/pkg/controllers"
 	"github.com/CESARBR/knot-cloud-storage/pkg/data"
 	"github.com/CESARBR/knot-cloud-storage/pkg/interactor"
 	"github.com/CESARBR/knot-cloud-storage/pkg/logging"
+	"github.com/CESARBR/knot-cloud-storage/pkg/network"
 	"github.com/CESARBR/knot-cloud-storage/pkg/server"
 )
 
@@ -16,16 +19,53 @@ func main() {
 	logger := logrus.Get("Main")
 	logger.Info("Starting KNoT Cloud Storage")
 
+	quit := make(chan bool, 1)
+
 	mongo := data.NewMongoDB(config.MongoDB.Host, config.MongoDB.Name)
 	database, err := mongo.Connect()
 	if err != nil {
 		logger.Error(err)
 	}
 
+	amqpStartChan := make(chan bool, 1)
+	amqp := network.NewAmqp(config.RabbitMQ.URL, logrus.Get("AMQP"))
+
 	dataStore := data.NewDataStore(database, logrus.Get("Storage"))
 	dataInteractor := interactor.NewDataInteractor(dataStore, logrus.Get("Interactor"))
 	dataController := controllers.NewDataController(dataInteractor, logrus.Get("Controller"))
 
+	handlerStartChan := make(chan bool, 1)
+	handler := server.NewHandler(logrus.Get("Handler"), amqp, dataInteractor)
+
+	serverStartChan := make(chan bool, 1)
 	server := server.NewServer(config.Server.Port, logrus.Get("Server"), dataController)
-	server.Start()
+
+	go amqp.Start(amqpStartChan)
+	go server.Start(serverStartChan)
+
+	// Main loop
+	for {
+		select {
+		case started := <-serverStartChan:
+			if started {
+				logger.Info("Server started")
+			}
+		case started := <-amqpStartChan:
+			if started {
+				logger.Info("AMQP connection started")
+				go handler.Start(handlerStartChan)
+			}
+		case started := <-handlerStartChan:
+			if started {
+				logger.Info("Handler started")
+			} else {
+				quit <- true
+			}
+		case <-quit:
+			handler.Stop()
+			amqp.Stop()
+			server.Stop()
+			os.Exit(0)
+		}
+	}
 }
