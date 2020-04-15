ab3819635dbc6bb4c5b1e81eafb658d0db3f315e
João Neto <joao.alexandre@cesar.org.br>
2020-04-15 11:42:18 -0300

    data: interactor: Add initial interface
    This change includes the initial step to refactor the interactors in a
    way that they become readable and easy to change. It adds an interface
    to abstract the use case operations and group it in a single file along
    with the `DataInteractor` structure and its constructor. The operations
    will be incrementally updated to be in accordance with the interface.
    
    Besides, this patch also removes unnecessary dot import from the
    interactor.

---
+++ pkg/interactor/interactor.go
@@ -0,0 +1,28 @@
+package interactor
+
+import (
+	"time"
+
+	"github.com/CESARBR/knot-cloud-storage/pkg/data"
+	"github.com/CESARBR/knot-cloud-storage/pkg/entities"
+	"github.com/CESARBR/knot-cloud-storage/pkg/logging"
+	"github.com/CESARBR/knot-cloud-storage/pkg/users"
+)
+
+// Interactor is an interface that defines the data's use cases operations
+type Interactor interface {
+	Save(token, id string, data []entities.Payload, time time.Time) error
+	List(token string, query *entities.Query) ([]entities.Data, error)
+}
+
+// DataInteractor represents the data layer interactor structure
+type DataInteractor struct {
+	UsersService users.Authenticator
+	DataStore    *data.DataStore
+	logger       logging.Logger
+}
+
+// NewDataInteractor creates a new data interactor instance
+func NewDataInteractor(users users.Authenticator, dataStore *data.DataStore, logger logging.Logger) *DataInteractor {
+	return &DataInteractor{users, dataStore, logger}
+}
