263fc95e9f9abc3c5f053ffe1abea3c4577cc313
João Neto <joao.alexandre@cesar.org.br>
2020-04-15 11:42:18 -0300

    data: interactor: Add initial interface
    This change includes the initial step to refactor the interactors in a
    way that they become readable and easy to change. It adds an interface
    to abstract the use case operations and group it in a single file along
    with the `DataInteractor` structure and its constructor. The operations
    will be incrementally updated to be in accordance with the interface.
    
    Besides, this patch also removes unnecessary dot import from the
    interactor.

--- pkg/interactor/data.go
+++ pkg/interactor/data.go
@@ -5,27 +5,11 @@ import (
 	"strconv"
 	"time"
 
-	"github.com/CESARBR/knot-cloud-storage/pkg/data"
 	"github.com/CESARBR/knot-cloud-storage/pkg/entities"
-	. "github.com/CESARBR/knot-cloud-storage/pkg/entities"
-	"github.com/CESARBR/knot-cloud-storage/pkg/logging"
-	"github.com/CESARBR/knot-cloud-storage/pkg/users"
 )
 
-// DataInteractor represents the data layer interactor structure
-type DataInteractor struct {
-	UsersService users.Authenticator
-	DataStore    *data.DataStore
-	logger       logging.Logger
-}
-
-// NewDataInteractor creates a new data interactor instance
-func NewDataInteractor(usersService users.Authenticator, dataStore *data.DataStore, logger logging.Logger) *DataInteractor {
-	return &DataInteractor{usersService, dataStore, logger}
-}
-
 // GetAll retrieves all the data present in the storage
-func (d *DataInteractor) GetAll(token string, query *entities.Query) ([]Data, error) {
+func (d *DataInteractor) GetAll(token string, query *entities.Query) ([]entities.Data, error) {
 	err := d.Authenticate(token)
 	if err != nil {
 		return nil, err
@@ -44,7 +28,7 @@ func (d *DataInteractor) GetAll(token string, query *entities.Query) ([]Data, er
 }
 
 // GetByID retrieves data by it's ID from the storage, if present
-func (d *DataInteractor) GetByID(token string, query *entities.Query) ([]Data, error) {
+func (d *DataInteractor) GetByID(token string, query *entities.Query) ([]entities.Data, error) {
 	err := d.Authenticate(token)
 	if err != nil {
 		return nil, err
@@ -74,7 +58,7 @@ func (d *DataInteractor) Save(token, id string, data []entities.Payload, timesta
 	}
 
 	for _, dt := range data {
-		err = d.DataStore.Save(Data{From: id, Payload: dt, Timestamp: timestamp})
+		err = d.DataStore.Save(entities.Data{From: id, Payload: dt, Timestamp: timestamp})
 		if err != nil {
 			return fmt.Errorf("error saving data %v: %w", data, err)
 		}
@@ -92,8 +76,8 @@ func (d *DataInteractor) Authenticate(token string) error {
 	return nil
 }
 
-func filterDataBySensorID(data []Data, sensorId int) []Data {
-	filteredData := make([]Data, 0)
+func filterDataBySensorID(data []entities.Data, sensorId int) []entities.Data {
+	filteredData := make([]entities.Data, 0)
 	for _, v := range data {
 		if v.Payload.SensorId == sensorId {
 			filteredData = append(filteredData, v)
