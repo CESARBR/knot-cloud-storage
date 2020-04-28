c0ea410e4458b7a1a6ba8cad30a64c80fb8683ac
Igor Albanezi Conceição <iac@cesar.org.br>
2020-04-28 11:19:32 -0300

    store: data: Change constructor return
    - Changes the return of the constructor, which was the struct,
      for the interface.
    - Changes the DataInteractor property type of DataStore for
      the interface returned by the constructor.
    - Removes a parameter from Save function since it's
      not necessary to be passed from the controller.
    - Adds .gitignore for vscode folder

--- pkg/interactor/save_data.go
+++ pkg/interactor/save_data.go
@@ -8,14 +8,14 @@ import (
 )
 
 // Save inserts data to the storage, if it doesn't exist already
-func (d *DataInteractor) Save(token, id string, data []entities.Payload, timestamp time.Time) error {
+func (d *DataInteractor) Save(token, id string, data []entities.Payload) error {
 	_, err := d.things.List(token)
 	if err != nil {
 		return err
 	}
 
 	for _, dt := range data {
-		err = d.DataStore.Save(entities.Data{From: id, Payload: dt, Timestamp: timestamp})
+		err = d.DataStore.Save(entities.Data{From: id, Payload: dt, Timestamp: time.Now()})
 		if err != nil {
 			return fmt.Errorf("error saving data %v: %w", data, err)
 		}
