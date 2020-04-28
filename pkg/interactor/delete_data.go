f100ec671579b28e843fd537e2f3e63a0f748e96
Igor Albanezi Conceição <iac@cesar.org.br>
2020-04-28 12:06:12 -0300

    interactor: delete: Add delete data test
    - Adds unit tests for the delete data feature

--- pkg/interactor/delete_data.go
+++ pkg/interactor/delete_data.go
@@ -1,5 +1,8 @@
 package interactor
 
+import "fmt"
+
+// Delete deletes data from the storage
 func (d *DataInteractor) Delete(token, deviceID string) error {
 	_, err := d.things.List(token)
 	if err != nil {
@@ -9,7 +12,7 @@ func (d *DataInteractor) Delete(token, deviceID string) error {
 	err = d.DataStore.Delete(deviceID)
 	if err != nil {
 		d.logger.Error(err)
-		return err
+		return fmt.Errorf("error deleting data from %v: %w", deviceID, err)
 	}
 	return nil
 }
