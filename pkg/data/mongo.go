933820b62b5558611f4b6f054e19497fa22636dc
Igor Albanezi Conceição <iac@cesar.org.br>
2020-05-08 14:46:59 -0300

    mongo: Increase the timeout for a connection
    In our scenario, working with docker containers, the default timeout
    in the driver wasn't going to be the ideal. For this reason, we
    increased the time to 60 seconds.
    
    - Adds the connection timeout configuration

--- pkg/data/mongo.go
+++ pkg/data/mongo.go
@@ -2,11 +2,15 @@ package data
 
 import (
 	"context"
+	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
+
+// CONNECTIONTIMEOUT is a constant to set the waiting time for a database connection
+const connectionTimeout = 60 * time.Second
 
 type MongoDB struct {
 	Server   string
@@ -18,7 +22,9 @@ func NewMongoDB(server string, database string) *MongoDB {
 }
 
 func (s *MongoDB) Connect() (*mongo.Database, error) {
-	clientOptions := options.Client().ApplyURI(s.Server)
+	clientOptions := options.Client()
+	clientOptions.ApplyURI(s.Server)
+	clientOptions.SetConnectTimeout(connectionTimeout)
 	client, err := mongo.NewClient(clientOptions)
 	if err != nil {
 		return nil, err
