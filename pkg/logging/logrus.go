4104b1a0f77b44e2dbcaf98abe41481e36c54c6a
João Neto <joao.alexandre@cesar.org.br>
2020-07-22 10:28:16 -0300

    logger: Enable syslog via unix socket
    In order to facilitate the service monitoring when running on the RPi
    this patch enables redirecting logs to the syslog via unix socket
    protocol.

--- pkg/logging/logrus.go
+++ pkg/logging/logrus.go
@@ -1,19 +1,22 @@
 package logging
 
 import (
+	"log/syslog"
 	"os"
 
 	"github.com/sirupsen/logrus"
+	logrus_syslog "github.com/sirupsen/logrus/hooks/syslog"
 )
 
 // Logrus represents the logrus logger
 type Logrus struct {
-	level string
+	level  string
+	syslog bool
 }
 
 // NewLogrus creates a new logrus instance
-func NewLogrus(level string) *Logrus {
-	return &Logrus{level: level}
+func NewLogrus(level string, syslog bool) *Logrus {
+	return &Logrus{level, syslog}
 }
 
 // Get returns a logrus instance based on the specific context
@@ -27,9 +30,27 @@ func (l *Logrus) Get(context string) *logrus.Entry {
 		TimestampFormat: "2006-01-02 15:04:05",
 	})
 
+	if l.syslog {
+		setupSyslog(log)
+	}
+
 	logger := log.WithFields(logrus.Fields{
 		"Context": context,
 	})
 
 	return logger
+}
+
+func setupSyslog(log *logrus.Logger) {
+	hook, err := logrus_syslog.NewSyslogHook("", "", syslog.LOG_INFO, "storage")
+	if err != nil {
+		log.Error("Unable to connect to local syslog daemon")
+		return
+	}
+
+	log.AddHook(hook)
+	log.SetFormatter(&logrus.TextFormatter{
+		DisableColors: true,
+		FullTimestamp: false,
+	})
 }
