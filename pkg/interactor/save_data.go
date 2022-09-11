d4b78aad1bf96a005da0e67fd8454de00166fcd6
Janael Pinheiro <ajp@cesar.org.br>
2022-09-11 19:27:37 -0300

    Reduce requests to Mainflux

--- pkg/interactor/save_data.go
+++ pkg/interactor/save_data.go
@@ -8,17 +8,23 @@ import (
 
 // Save inserts data to the storage, if it doesn't exist already
 func (d *DataInteractor) Save(token, id string, data []entities.Payload) error {
-	_, err := d.things.List(token)
-	if err != nil {
-		return fmt.Errorf("%v: %w", ErrValidToken, err)
-	}
-
-	for _, dt := range data {
-		err = d.DataStore.Save(entities.Data{From: id, Payload: dt})
+	_, ok := d.tokenCache[token]
+	if ok {
+		return nil
+	} else {
+		_, err := d.things.List(token)
 		if err != nil {
-			return fmt.Errorf("error saving data %v: %w", data, err)
+			return fmt.Errorf("%v: %w", ErrValidToken, err)
 		}
-	}
+		// Updates the cache to signal the existence of the token.
+		d.tokenCache[token] = struct{}{}
+		for _, dt := range data {
+			err = d.DataStore.Save(entities.Data{From: id, Payload: dt})
+			if err != nil {
+				return fmt.Errorf("error saving data %v: %w", data, err)
+			}
+		}
 
-	return nil
+		return nil
+	}
 }
