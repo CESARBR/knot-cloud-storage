b54a982f1029864fa66f450eaf7de2656bf351cc
rtorresmatheus <mtr@cesar.org.br>
2022-10-19 22:39:50 -0300

    Fix condition for save data

--- pkg/interactor/save_data.go
+++ pkg/interactor/save_data.go
@@ -9,22 +9,19 @@ import (
 // Save inserts data to the storage, if it doesn't exist already
 func (d *DataInteractor) Save(token, id string, data []entities.Payload) error {
 	_, ok := d.tokenCache[token]
-	if ok {
-		return nil
-	} else {
+	if !ok {
 		_, err := d.things.List(token)
 		if err != nil {
 			return fmt.Errorf("%v: %w", ErrValidToken, err)
 		}
-		// Updates the cache to signal the existence of the token.
 		d.tokenCache[token] = struct{}{}
-		for _, dt := range data {
-			err = d.DataStore.Save(entities.Data{From: id, Payload: dt})
-			if err != nil {
-				return fmt.Errorf("error saving data %v: %w", data, err)
-			}
-		}
-
-		return nil
 	}
+	// Updates the cache to signal the existence of the token.
+	for _, dt := range data {
+		err := d.DataStore.Save(entities.Data{From: id, Payload: dt})
+		if err != nil {
+			return fmt.Errorf("error saving data %v: %w", data, err)
+		}
+	}
+	return nil
 }
